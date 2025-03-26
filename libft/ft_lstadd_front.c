7bd9af018be52b68c13db3b18f96f333ec0492fe
Your Name <you@example.com>
2025-03-26 14:58:15 +0100

    Force add libft to feat/lexer-parser from master

---
+++ libft/ft_lstadd_front.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhuthmay <mhuthmay@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 11:36:52 by mhuthmay          #+#    #+#             */
+/*   Updated: 2024/09/16 14:19:24 by mhuthmay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!new)
+		return ;
+	if (!lst)
+	{
+		*lst = new;
+		return ;
+	}
+	new->next = *lst;
+	*lst = new;
+}

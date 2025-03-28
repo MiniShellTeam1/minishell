a45d98a187375190435d7a09201309a9e360ca58
ncantona <nico.cantonati@outlook.com>
2025-03-28 22:26:13 +0100

    added header to define error messages

---
+++ errormsg.h
@@ -0,0 +1,12 @@
+#ifndef ERRORMSG_H
+# define ERRORMSG_H
+
+# define NO_SUCH_FILE_OR_DIRECTORY "No such file or directory\n"
+# define PERMISSION_DENIED "Permission denied\n"
+# define NOT_A_DIRECTORY "Not a directory\n"
+# define TOO_MANY_ARGUMENTS "too many arguments\n"
+# define ARGUMENT_LIST_TOO_LONG "Argument list too long\n"
+# define TOO_MANY_LEVELS_OF_SYMB_LINK "Too many levels of symbolic links\n"
+# define FILE_NAME_TOO_LONG "File name too long\n"
+
+#endif

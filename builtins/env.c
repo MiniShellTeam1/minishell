d4a3839c94d4ed016496b868f667b1090228dcf5
Nico Cantonati <ncantona@c3r10p6.42vienna.com>
2025-04-18 18:21:58 +0200

    outcommented the first if condition in env

--- builtins/env.c
+++ builtins/env.c
@@ -4,12 +4,12 @@
 /* prints out the whole enviroment */
 
 void ft_env(t_master master)
-{
-/* 	if (master.cmds->args[1])
+{ 
+ 	if (master.cmds->args[1])
 	{
 		master.errorcode = 1;
 		return ;
-	} */
+	}
 	while (master.env)
 	{
 		ft_putstr_fd(master.env->key, 1);
